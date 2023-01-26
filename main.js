e98bdab762d71b8ec8ea3a4cb0fcced210973bee
Lenka Polcerova <lenka.polcerova@gmail.com>
2023-01-26 22:26:33 +0100

    splneni ukolu

---
+++ main.js
@@ -0,0 +1,28 @@
+let odstavec = document.querySelector("p");
+let zvuk = document.querySelector(".zvuk");
+
+
+
+function textBold(){
+    odstavec.style.fontWeight = "bold";
+}
+
+function zmenBarvu(){
+    odstavec.classList.add("cerveny");
+}
+
+function zvetsiPismo(){
+    odstavec.style.fontSize = "17px";
+}
+
+function prehraj(){
+    zvuk.play();
+}
+
+function stop(){
+    zvuk.pause();
+}
+
+function pretoc(){
+    zvuk.currentTime=0;
+}
